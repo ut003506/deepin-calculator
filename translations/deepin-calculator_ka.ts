8575a164a2e2dbb807557842f38d0c559e96ff9a
jingzhou <jingzhou@uniontech.com>
2021-07-20 13:28:14 +0800

    chore: 更新翻译
    更新翻译
    
    Log: 更新翻译
    Change-Id: If77ea2f1b4c87421642dec48f468a055d280343f

--- translations/deepin-calculator_ka.ts
+++ translations/deepin-calculator_ka.ts
@@ -1,11 +1,11 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ka" version="2.1">
 <context>
     <name>ExpressionBar</name>
     <message>
         <location filename="src/widgets/expressionbar.cpp" line="786"/>
         <location filename="src/widgets/expressionbar.cpp" line="1392"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
         <location filename="src/tablet/widgets/expressionbartab.cpp" line="788"/>
         <location filename="src/tablet/widgets/expressionbartab.cpp" line="1394"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="src/control/iconbutton.cpp" line="123"/>
         <source>Clear all memory</source>
-        <translation>清除所有内存</translation>
+        <translation>მეხსიერების გასუფთავება</translation>
     </message>
     <message>
         <location filename="src/control/iconbutton.cpp" line="126"/>
         <source>Clear history</source>
-        <translation>清除历史记录</translation>
+        <translation>ისტორიის გასუფთავება</translation>
     </message>
 </context>
 <context>
@@ -35,37 +35,37 @@
     <message>
         <location filename="src/widgets/inputedit.cpp" line="282"/>
         <source>Undo</source>
-        <translation>撤销</translation>
+        <translation>დაბრუნება</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="283"/>
         <source>Redo</source>
-        <translation>恢复</translation>
+        <translation>წინ</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="284"/>
         <source>Cut</source>
-        <translation>剪切</translation>
+        <translation>ამოჭრა</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="285"/>
         <source>Copy</source>
-        <translation>复制</translation>
+        <translation>კოპირება</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="286"/>
         <source>Paste</source>
-        <translation>粘贴</translation>
+        <translation>ჩასმა</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="287"/>
         <source>Delete</source>
-        <translation>删除</translation>
+        <translation>წაშლა</translation>
     </message>
     <message>
         <location filename="src/widgets/inputedit.cpp" line="288"/>
         <source>Select All</source>
-        <translation>全选</translation>
+        <translation>ყველას მონიშვნა</translation>
     </message>
 </context>
 <context>
@@ -74,32 +74,32 @@
         <location filename="src/main.cpp" line="115"/>
         <location filename="src/mainwindow.cpp" line="82"/>
         <source>Calculator</source>
-        <translation>计算器</translation>
+        <translation>კალკულატორი</translation>
     </message>
     <message>
         <location filename="src/main.cpp" line="117"/>
         <source>Calculator is an easy to use desktop calculator, supporting standard and scientific modes.</source>
-        <translation>计算器是一款简单易用的桌面计算器，支持标准型和科学型模式。</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="57"/>
         <source>Standard</source>
-        <translation>标准</translation>
+        <translation>სტანდარტული</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="58"/>
         <source>Scientific</source>
-        <translation>科学</translation>
+        <translation>სამეცნიერო</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="59"/>
         <source>History</source>
-        <translation>历史记录</translation>
+        <translation>ისოტირა</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="68"/>
         <source>Mode</source>
-        <translation>模式</translation>
+        <translation>რეჟიმი</translation>
     </message>
 </context>
 <context>
@@ -107,17 +107,17 @@
     <message>
         <location filename="src/tablet/mainwindowtab.cpp" line="58"/>
         <source>Standard</source>
-        <translation>标准</translation>
+        <translation>სტანდარტული</translation>
     </message>
     <message>
         <location filename="src/tablet/mainwindowtab.cpp" line="59"/>
         <source>Scientific</source>
-        <translation>科学</translation>
+        <translation>სამეცნიერო</translation>
     </message>
     <message>
         <location filename="src/tablet/mainwindowtab.cpp" line="83"/>
         <source>Calculator</source>
-        <translation>计算器</translation>
+        <translation>კალკულატორი</translation>
     </message>
 </context>
 <context>
@@ -125,47 +125,47 @@
     <message>
         <location filename="src/control/memorybutton.cpp" line="107"/>
         <source>Clear memory item</source>
-        <translation>清除内存项</translation>
+        <translation>მეხსიერების აითემის გასუფთავება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="109"/>
         <source>Clear all memory</source>
-        <translation>清除所有内存</translation>
+        <translation>მეხსიერების გასუფთავება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="111"/>
         <source>Memory recall</source>
-        <translation>内存重新调用</translation>
+        <translation>მეხსიერების გახსენება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="114"/>
         <source>Add to memory item</source>
-        <translation>添加到内存项</translation>
+        <translation>მეხისერებაში დამატება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="116"/>
         <source>Memory add</source>
-        <translation>内存增加</translation>
+        <translation>მეხსიერების დამატება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="119"/>
         <source>Subtract from memory item</source>
-        <translation>从内存项中减掉</translation>
+        <translation>მეხსიერებიდან ამოღება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="121"/>
         <source>Memory subtract</source>
-        <translation>内存减少</translation>
+        <translation>მეხსიერების გამოკლება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="123"/>
         <source>Memory store</source>
-        <translation>内存存储</translation>
+        <translation>მეხსიერების განთავსება</translation>
     </message>
     <message>
         <location filename="src/control/memorybutton.cpp" line="125"/>
         <source>Memory</source>
-        <translation>内存</translation>
+        <translation>მეხსიერება</translation>
     </message>
 </context>
 <context>
@@ -174,25 +174,25 @@
         <location filename="src/views/memoryitemwidget.cpp" line="148"/>
         <location filename="src/views/memoryitemwidget.cpp" line="240"/>
         <source>Copy</source>
-        <translation>复制</translation>
+        <translation>კოპირება</translation>
     </message>
     <message>
         <location filename="src/views/memoryitemwidget.cpp" line="149"/>
         <location filename="src/views/memoryitemwidget.cpp" line="241"/>
         <source>Clear memory item</source>
-        <translation>清除内存项</translation>
+        <translation>მეხსიერების აითემის გასუფთავება</translation>
     </message>
     <message>
         <location filename="src/views/memoryitemwidget.cpp" line="150"/>
         <location filename="src/views/memoryitemwidget.cpp" line="242"/>
         <source>Add to memory item</source>
-        <translation>添加到内存项</translation>
+        <translation>მეხისერებაში დამატება</translation>
     </message>
     <message>
         <location filename="src/views/memoryitemwidget.cpp" line="151"/>
         <location filename="src/views/memoryitemwidget.cpp" line="243"/>
         <source>Subtract from memory item</source>
-        <translation>从内存项中减掉</translation>
+        <translation>მეხსიერებიდან ამოღება</translation>
     </message>
 </context>
 <context>
@@ -201,25 +201,25 @@
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="148"/>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="241"/>
         <source>Copy</source>
-        <translation>复制</translation>
+        <translation>კოპირება</translation>
     </message>
     <message>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="149"/>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="242"/>
         <source>Clear memory item</source>
-        <translation>清除内存项</translation>
+        <translation>მეხსიერების აითემის გასუფთავება</translation>
     </message>
     <message>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="150"/>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="243"/>
         <source>Add to memory item</source>
-        <translation>添加到内存项</translation>
+        <translation>მეხისერებაში დამატება</translation>
     </message>
     <message>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="151"/>
         <location filename="src/tablet/views/memoryitemwidgettab.cpp" line="244"/>
         <source>Subtract from memory item</source>
-        <translation>从内存项中减掉</translation>
+        <translation>მეხსიერებიდან ამოღება</translation>
     </message>
 </context>
 <context>
@@ -227,7 +227,7 @@
     <message>
         <location filename="src/views/memorywidget.cpp" line="148"/>
         <source>Nothing saved in memory</source>
-        <translation>未保存数据</translation>
+        <translation>მეხსიერება ცარიელია</translation>
     </message>
 </context>
 <context>
@@ -235,7 +235,7 @@
     <message>
         <location filename="src/tablet/views/memorywidgettab.cpp" line="130"/>
         <source>Nothing saved in memory</source>
-        <translation>未保存数据</translation>
+        <translation>მეხსიერება ცარიელია</translation>
     </message>
 </context>
 <context>
@@ -243,7 +243,7 @@
     <message>
         <location filename="src/main.cpp" line="121"/>
         <source>Calculator</source>
-        <translation>计算器</translation>
+        <translation>კალკულატორი</translation>
     </message>
 </context>
 <context>
@@ -252,7 +252,7 @@
         <location filename="src/widgets/sciexpressionbar.cpp" line="494"/>
         <location filename="src/widgets/sciexpressionbar.cpp" line="540"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -261,7 +261,7 @@
         <location filename="src/tablet/widgets/sciexpressionbartab.cpp" line="495"/>
         <location filename="src/tablet/widgets/sciexpressionbartab.cpp" line="541"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -269,12 +269,12 @@
     <message>
         <location filename="src/widgets/scihistorywidget.cpp" line="51"/>
         <source>History</source>
-        <translation>历史记录</translation>
+        <translation>ისოტირა</translation>
     </message>
     <message>
         <location filename="src/widgets/scihistorywidget.cpp" line="52"/>
         <source>Memory</source>
-        <translation>内存</translation>
+        <translation>მეხსიერება</translation>
     </message>
 </context>
 <context>
@@ -283,7 +283,7 @@
         <location filename="src/views/simplelistdelegate.cpp" line="194"/>
         <location filename="src/views/simplelistdelegate.cpp" line="338"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -292,7 +292,7 @@
         <location filename="src/tablet/views/simplelistdelegatetab.cpp" line="194"/>
         <location filename="src/tablet/views/simplelistdelegatetab.cpp" line="338"/>
         <source>Expression error</source>
-        <translation>表达式错误</translation>
+        <translation>გამოსახვის შეცდომა</translation>
     </message>
 </context>
 <context>
@@ -300,7 +300,7 @@
     <message>
         <location filename="src/views/simplelistmodel.cpp" line="108"/>
         <source>No history</source>
-        <translation>无历史记录</translation>
+        <translation>ისტორია არ არის</translation>
     </message>
 </context>
 <context>
@@ -309,13 +309,13 @@
         <location filename="src/views/simplelistview.cpp" line="71"/>
         <location filename="src/views/simplelistview.cpp" line="99"/>
         <source>Copy</source>
-        <translation>复制</translation>
+        <translation>კოპირება</translation>
     </message>
     <message>
         <location filename="src/views/simplelistview.cpp" line="72"/>
         <location filename="src/views/simplelistview.cpp" line="100"/>
         <source>Delete</source>
-        <translation>删除</translation>
+        <translation>წაშლა</translation>
     </message>
 </context>
 <context>
@@ -324,13 +324,13 @@
         <location filename="src/tablet/views/simplelistviewtab.cpp" line="70"/>
         <location filename="src/tablet/views/simplelistviewtab.cpp" line="98"/>
         <source>Copy</source>
-        <translation>复制</translation>
+        <translation>კოპირება</translation>
     </message>
     <message>
         <location filename="src/tablet/views/simplelistviewtab.cpp" line="71"/>
         <location filename="src/tablet/views/simplelistviewtab.cpp" line="99"/>
         <source>Delete</source>
-        <translation>删除</translation>
+        <translation>წაშლა</translation>
     </message>
 </context>
 </TS>
